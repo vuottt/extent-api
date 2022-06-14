0f67335c848cdb722e840690dcc2d29f45beb440
vuottt <88264740+vuottt@users.noreply.github.com>
2022-06-14 15:24:37 +0700

    update

---
+++ class/CpanelClass.php
@@ -0,0 +1,85 @@
+<?php
+    function createAccount ($newDomain = "", $newUser = "", $newPass = "", $newPlan = "", $newEmail = "") {
+        $whm_user = "vuottt";
+        $whm_pass = "vinahost@888";
+        $whm_host = "em.vinahost.vn";
+
+        $newDomain = fixData($newDomain);
+        $newUser = fixData($newUser);
+        $newPlan = fixData($newPlan);
+
+        If (!empty($newUser)) {
+            // Get new session ID
+            $sessID = newSession($whm_host, $whm_user, $whm_pass);
+
+            $script = "https://www." . $whm_host . ":2087" . $sessID . "/scripts/wwwacct";
+            $sData = "?plan={$newPlan}&domain={$newDomain}&username={$newUser}&password={$newPass}&contactemail={$newEmail}";
+
+            // Authenticate User for WHM access
+            $context = stream_context_create(array(
+                'http' => array(
+                    'header' => "Authorization: Basic " . base64_encode("{$whm_user}:{$whm_pass}"),
+                ),
+            ));
+
+            // Get result
+            $result = file_get_contents($script.$sData, false, $context);
+            //Echo "URL: " . $script.$sData . "<br /><br />";
+            // Echo "Result: " . $result;
+
+        } Else {
+            Echo "Error: Username is empty!";
+        }
+    }
+
+
+    Function newSession($nHost, $nUser, $nPass) { // Example details
+        $ip = $nHost;
+        $cp_user = $nUser;
+        $cp_pwd = $nPass;
+        $url = "https://$ip:2087/login";
+
+        // Create new curl handle
+        $ch=curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "user=$cp_user&pass=$cp_pwd");
+        curl_setopt($ch, CURLOPT_TIMEOUT, 100020);
+
+        // Execute the curl handle and fetch info then close streams.
+        $f = curl_exec($ch);
+        $h = curl_getinfo($ch);
+        curl_close($ch);
+
+        // If we had no issues then try to fetch the cpsess
+        if ($f == true and strpos($h['url'],"cpsess"))
+        {
+            // Get the cpsess part of the url
+            $pattern="/.*?(\/cpsess.*?)\/.*?/is";
+            $preg_res=preg_match($pattern,$h['url'],$cpsess);
+        }
+
+        // If we have a session then return it otherwise return empty string
+        return (isset($cpsess[1])) ? $cpsess[1] : "";
+    }
+
+    Function fixData($data) {
+        $data = str_replace("-","%2D",$data);
+        $data = str_replace(".","%2E",$data);
+        $data = str_replace(" ","%20",$data);
+
+        return $data;
+    }
+
+    Function returnData($data) {
+        $data = str_replace("%2D","-",$data);
+        $data = str_replace("%2E",".",$data);
+        $data = str_replace("%20"," ",$data);
+
+        return $data;
+    }
+    header('Location: https://www.example.com/users');
+?>
--- items/create.php
+++ items/create.php
@@ -7,32 +7,47 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
  
 include_once '../config/Database.php';
 include_once '../class/Items.php';
+include_once '../class/CpanelClass.php';
+include_once '../class/CpanelItems.php';
+
+// $database = new Database();
+// $db = $database->getConnection();
  
-$database = new Database();
-$db = $database->getConnection();
- 
-$items = new Items($db);
+// $items = new Items($db);
  
 $data = json_decode(file_get_contents("php://input"));
+print_r($data);
+// if(!empty($data->name) && !empty($data->description) &&
+// !empty($data->price) && !empty($data->category_id) &&
+// !empty($data->created)){    
 
-if(!empty($data->name) && !empty($data->description) &&
-!empty($data->price) && !empty($data->category_id) &&
-!empty($data->created)){    
-
-    $items->name = $data->name;
-    $items->description = $data->description;
-    $items->price = $data->price;
-    $items->category_id = $data->category_id;	
-    $items->created = date('Y-m-d H:i:s'); 
+//     $items->name = $data->name;
+//     $items->description = $data->description;
+//     $items->price = $data->price;
+//     $items->category_id = $data->category_id;	
+//     $items->created = date('Y-m-d H:i:s'); 
     
-    if($items->create()){         
-        http_response_code(201);         
-        echo json_encode(array("message" => "Item was created."));
-    } else{         
-        http_response_code(503);        
-        echo json_encode(array("message" => "Unable to create item."));
-    }
-}else{    
+//     if($items->create()){         
+//         http_response_code(201);         
+//         echo json_encode(array("message" => "Item was created."));
+//     } else{         
+//         http_response_code(503);        
+//         echo json_encode(array("message" => "Unable to create item."));
+//     }
+// }else{    
+//     http_response_code(400);    
+//     echo json_encode(array("message" => "Unable to create item. Data is incomplete."));
+// }
+if(!empty($data->newDomain) && !empty($data->newUser) && !empty($data->newPass) && !empty($data->newPlan) && !empty($data->newEmail)) {
+
+    $domain = $data->newDomain;
+    $user = $data->newUser;
+    $pasword = $data->newPass;
+    $plan = $data->newPlan;
+    $email = $data->newEmail;
+    createAccount ($newDomain = "$domain", $newUser = "$user", $newPass = "$pasword", $newPlan = "$plan", $newEmail = "$email");
+}
+else{    
     http_response_code(400);    
     echo json_encode(array("message" => "Unable to create item. Data is incomplete."));
 }
